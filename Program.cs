ce4e30a5f428b1fd25113e8f9fad50cb8ffccd52
VelugotiKavyaTharuni9490 <kavyatharunivelugoti@gmail.com>
2024-06-28 07:38:20 +0530

    PhoneBookAPP
    Creating a phone book management application using ASP.NET Core MVC involves setting up the project structure, creating models, controllers, and views to handle CRUD operations. Below is a step-by-step guide to create such an application.

--- Program.cs
+++ Program.cs
@@ -1,44 +1,32 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using PhoneBookApp.Data;
 
-namespace BookStore
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<PhoneBookContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-          var host =CreateHostBuilder(args).Build();
-          using (var scope =host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    SeedData.IniTialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex,  "An error occured while attempting to seed the database");
-                }
-            }
-            host.Run();
-        }
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-
-    internal class Startup
-    {
-    }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
